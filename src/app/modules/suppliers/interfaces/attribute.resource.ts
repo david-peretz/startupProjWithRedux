e1235da8a5774f0856cd9bed7984e9b8da8df1d5
david-peretz <david82761@gmail.com>
2020-02-05 23:35:04 +0200

    Auto-generated commit

--- src/app/modules/suppliers/interfaces/attribute.resource.ts
+++
@@ -1,5 +0,0 @@
-import { Resource } from "src/app/core/services/generic/resource";
-
-export class Attribute extends Resource {
-  value: string = "";
-}
--- src/app/modules/suppliers/interfaces/supplier.resource.ts
+++
@@ -1,9 +0,0 @@
-import { Resource } from "src/app/core/services/generic/resource";
-
-export class Supplier extends Resource {
-  name: string = "";
-  address: string = "";
-  phone: string = "";
-  email: string = "";
-  attributes: number[] = [];
-}
